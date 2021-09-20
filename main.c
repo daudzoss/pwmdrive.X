998487430e43e1b92e5d760625acd77dd8f9381e
Daud Zoss <daud@vence.io>
2021-09-20 13:18:13 -0700

    about to ditch 8-bit types

--- main.c
+++ main.c
@@ -1,47 +1,39 @@
-/**
-  Generated Main Source File
-
-  Company:
-    Microchip Technology Inc.
-
-  File Name:
-    main.c
-
-  Summary:
-    This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
-
-  Description:
-    This header file provides implementations for driver APIs for all modules selected in the GUI.
-    Generation Information :
-        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.81.7
-        Device            :  PIC16F18877
-        Driver Version    :  2.00
-*/
-
-/*
-    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
-    
-    Subject to your compliance with these terms, you may use Microchip software and any 
-    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
-    license terms applicable to your use of third party software (including open source software) that 
-    may accompany Microchip software.
-    
-    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
-    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
-    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
-    FOR A PARTICULAR PURPOSE.
-    
-    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
-    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
-    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
-    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
-    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
-    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
-    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
-    SOFTWARE.
-*/
-
 #include "mcc_generated_files/mcc.h"
+#include "stdint.h"
+
+#define NO_MODE_SELECTED 0
+#define FREQ_ADJUST_MODE 1
+#define DUTY_ADJUST_MODE 2
+
+void read_adc(int8_t* lbyte, uint8_t* hbyte)
+{
+  adc_result_t convertedValue;
+
+//  ADCC_StartConversion(TRIMPOT);
+//  while (!ADCC_IsConversionDone())
+//    ;
+
+  convertedValue = ADCC_GetConversionResult();
+  *lbyte = convertedValue & 0xff;
+  *hbyte = convertedValue >> 8;
+}
+
+void set_parameter(uint8_t parm, uint8_t lbyte, uint8_t hbyte)
+{
+  switch (parm) {
+  case FREQ_ADJUST_MODE:
+
+    break;
+    
+  case DUTY_ADJUST_MODE:
+
+    break;
+
+  case NO_MODE_SELECTED:
+  default:
+    return;
+  }
+}
 
 /*
                          Main application
@@ -51,26 +43,23 @@ void main(void)
     // initialize the device
     SYSTEM_Initialize();
 
-    // When using interrupts, you need to set the Global and Peripheral Interrupt Enable bits
-    // Use the following macros to:
-
-    // Enable the Global Interrupts
-    //INTERRUPT_GlobalInterruptEnable();
-
-    // Enable the Peripheral Interrupts
-    //INTERRUPT_PeripheralInterruptEnable();
-
     // Disable the Global Interrupts
     //INTERRUPT_GlobalInterruptDisable();
 
     // Disable the Peripheral Interrupts
     //INTERRUPT_PeripheralInterruptDisable();
 
-    while (1)
-    {
-        // Add your application code
+    uint8_t mode = NO_MODE_SELECTED;
+    uint8_t adc_lbyte, adc_hbyte;
+
+    while (1) {
+      if (S1PORT & S1MASK == 0) // S1 pressed
+	mode = FREQ_ADJUST_MODE;
+
+      else if (S2_PORT & S2MASK == 0) // S2 pressed
+	mode = DUTY_ADJUST_MODE;
+	  
+      read_adc(&adc_lbyte, &adc_hbyte);
+      set_parameter(mode, adc_lbyte, adc_hbyte);
     }
 }
-/**
- End of File
-*/
